d239bfa7cbac043f52fd73440503fe77be40a9a5
JM <jacobmontpetit@gmail.com>
2024-06-30 23:37:20 -0400

    Flake: add nix flake

---
+++ flake.nix
@@ -0,0 +1,11 @@
+{
+  inputs = { utils.url = "github:numtide/flake-utils"; };
+  outputs = { self, nixpkgs, utils }:
+    utils.lib.eachDefaultSystem (system:
+      let pkgs = nixpkgs.legacyPackages.${system};
+      in {
+        devShell = pkgs.mkShell {
+          buildInputs = with pkgs; [ cmake openocd gcc-arm-embedded ninja ];
+        };
+      });
+}
